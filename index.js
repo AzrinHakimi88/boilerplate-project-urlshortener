89016df64c215b0bad38c551bb71c119e4f6835c
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-03-28 23:35:49 +0800

    commit

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+let currentshortUrl = 1
+let urlMapping = {}
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +24,35 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+    const url = req.body.url;
+    try{
+      new URL(url)
+      urlMapping[currentshortUrl++] = url
+      res.json({ original_url : url, short_url : currentshortUrl - 1})
+    }
+    catch(err){
+      res.json({'error' : 'invalid url'})
+    }
+
+})
+
+
+app.get('/api/shorturl:shortUrl',function(req,res){
+    const shortUrl = req.params.shortUrl
+    
+    shortUrl = parseInt(shortUrl)
+    const originalUrl = urlMapping[shortUrl]
+    console.log(originalUrl)
+
+    if(originalUrl){
+      res.redirect(originalUrl)
+    } 
+    else{
+      res.json({ error: 'short url not found' });
+    }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
